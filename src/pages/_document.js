64ac5cf75332b02b82680e5ccc825e0fef479c19
pistochos <spameenmeaqui@gmail.com>
2025-09-24 10:08:19 -0600

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,25 +1,18 @@
-
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="title" content="PEREGRINO" />
-        <meta name="description" content="FIND A GREAT PLACE, WE'LL DO THE REST." />
-
+        {/* Meta globales */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://peregrinocompany.com/" />
-        <meta property="og:title" content="PEREGRINO" />
-        <meta property="og:description" content="FIND A GREAT PLACE, WE'LL DO THE REST." />
         <meta property="og:image" content="/meta-img.jpg" />
-
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://peregrinocompany.com/" />
-        <meta property="twitter:title" content="PEREGRINO" />
-        <meta property="twitter:description" content="FIND A GREAT PLACE, WE'LL DO THE REST." />
         <meta property="twitter:image" content="/meta-img.jpg" />
 
+        {/* Favicons */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -29,14 +22,16 @@ export default function Document() {
 
         {/* Google Ads Pixel */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17468723893"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17468723893');
-          `,
-        }} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17468723893');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
@@ -45,3 +40,4 @@ export default function Document() {
     </Html>
   )
 }
+
